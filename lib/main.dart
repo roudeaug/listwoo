6bcf1e0de01c7bd4d2389fa965429eb7ba49d0cc
g.roudeau@gmail.com <g.roudeau@gmail.com>
2021-11-12 16:35:28 +0100

    App naming

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ListwooApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ListwooApp extends StatelessWidget {
+  const ListwooApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
