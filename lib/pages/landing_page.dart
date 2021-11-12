69828e6c0b8a4752c5bb6b997edc381373c927eb
g.roudeau@gmail.com <g.roudeau@gmail.com>
2021-11-13 00:22:42 +0100

    Route configuration

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:listwoo/animations/delayed_animation.dart';
 import 'package:listwoo/config/color_chart.dart';
+import 'package:listwoo/pages/sign_in_page.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -11,55 +12,58 @@ class LandingPage extends StatelessWidget {
     return Scaffold(
         backgroundColor: ColorChart.background,
         body: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-             return SingleChildScrollView(
-              child: ConstrainedBox(
-              constraints: BoxConstraints(
-              minHeight: constraints.maxHeight),
+            builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
               child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 60, horizontal: 40),
+                margin:
+                    const EdgeInsets.symmetric(vertical: 60, horizontal: 40),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         children: [
                           DelayedAnimation(
-                            delay: 1000, 
-                            child: Container(
-                              margin: const EdgeInsets.only(bottom: 40),
-                              height: 230,
-                              child: SvgPicture.asset('assets/logos/logo_label.svg'),
-                            )
-                          ),
+                              delay: 1000,
+                              child: Container(
+                                margin: const EdgeInsets.only(bottom: 40),
+                                height: 230,
+                                child: SvgPicture.asset(
+                                    'assets/logos/logo_label.svg'),
+                              )),
                           DelayedAnimation(
-                            delay: 2000, 
+                            delay: 2000,
                             child: SizedBox(
-                              height: 250,
-                              child: Image.asset('assets/images/img4.png')
-                            ),
+                                height: 250,
+                                child: Image.asset('assets/images/img4.png')),
                           )
                         ],
                       ),
                       DelayedAnimation(
-                        delay: 2500, 
+                        delay: 2500,
                         child: SizedBox(
                           width: double.infinity,
                           height: 42,
                           child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
-                              primary: ColorChart.accent,
-                              shape: const StadiumBorder()),
+                                primary: ColorChart.accent,
+                                shape: const StadiumBorder()),
                             child: const Text("COMMENCER"),
-                            onPressed: () {},
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const SignInPage()),
+                              );
+                            },
                           ),
                         ),
                       ),
-                    ]
-                  ),
-                ),
+                    ]),
               ),
-            );
-        })
-    );
+            ),
+          );
+        }));
   }
 }
---
+++ lib/pages/sign_in_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:listwoo/config/color_chart.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorChart.background,
+      body: Container(),
+    );
+  }
+}
